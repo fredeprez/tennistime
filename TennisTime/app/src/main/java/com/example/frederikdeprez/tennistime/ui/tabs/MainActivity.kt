dc920d84b475dafbc5e3680a961839ee7d57ac3f
Frederik Deprez <deprez.frederik@gmail.com>
2018-12-20 23:05:14 +0100

    Add tabs layout with icons

--- TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/ui/tabs/MainActivity.kt
+++ TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/ui/tabs/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.frederikdeprez.tennistime
+package com.example.frederikdeprez.tennistime.ui.tabs
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.frederikdeprez.tennistime.R
 
 class MainActivity : AppCompatActivity() {
 
