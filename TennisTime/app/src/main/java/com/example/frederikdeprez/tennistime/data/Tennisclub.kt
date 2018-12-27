20dd0ed3904493163d983f9bfdd83d8d1d3a85b3
Frederik Deprez <deprez.frederik@gmail.com>
2018-12-27 20:45:29 +0100

    Update tennisclub model with list of players

--- TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/data/Tennisclub.kt
+++ TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/data/Tennisclub.kt
@@ -3,5 +3,6 @@ package com.example.frederikdeprez.tennistime.data
 data class Tennisclub(
         private val name: String,
         private val location: String,
-        private val vtvnumber: String
+        private val vtvnumber: String,
+        private val players: List<Player>
 )
