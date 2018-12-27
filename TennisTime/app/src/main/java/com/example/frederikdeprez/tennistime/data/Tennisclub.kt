d195a9d79acf3749945b915915f17432325d013f
Frederik Deprez <deprez.frederik@gmail.com>
2018-12-27 20:41:03 +0100

    Add Tennisclub model

---
+++ TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/data/Tennisclub.kt
@@ -0,0 +1,7 @@
+package com.example.frederikdeprez.tennistime.data
+
+data class Tennisclub(
+        private val name: String,
+        private val location: String,
+        private val vtvnumber: String
+)
