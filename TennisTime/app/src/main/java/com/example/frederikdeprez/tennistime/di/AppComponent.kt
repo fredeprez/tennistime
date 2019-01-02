4053b2849cebca177a95aed792f7963d429cdc25
Frederik Deprez <deprez.frederik@gmail.com>
2019-01-02 06:03:25 +0100

    Add AppComponent for injection

---
+++ TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/di/AppComponent.kt
@@ -0,0 +1,10 @@
+package com.example.frederikdeprez.tennistime.di
+
+import android.app.Application
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component()
+interface AppComponent {
+}
