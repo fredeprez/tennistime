3ea9b309357747534cfffb2ae9e025f3c792e629
Frederik Deprez <deprez.frederik@gmail.com>
2019-01-03 02:41:31 +0100

    Update ids to standard

--- TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/data/Player.kt
+++ TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/data/Player.kt
@@ -3,7 +3,7 @@ package com.example.frederikdeprez.tennistime.data
 import java.util.*
 
 data class Player(
-        private val player_id: Long,
+        private val playerId: Long,
         private val name: String,
         private val email: String,
         private val phonenumber: String,
