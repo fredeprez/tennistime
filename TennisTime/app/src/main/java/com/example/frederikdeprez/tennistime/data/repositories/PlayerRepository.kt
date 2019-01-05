3766a18fa6058bc73308d69e2b5a606878701adc
Frederik Deprez <deprez.frederik@gmail.com>
2019-01-05 05:44:19 +0100

    Update MatchViewModel having mutable list

--- TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/data/repositories/PlayerRepository.kt
+++ TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/data/repositories/PlayerRepository.kt
@@ -12,7 +12,7 @@ class PlayerRepository: InjectableRepository() {
     /**
      * Get all the [Player] from [API] from specific [Tennisclub]
      */
-    fun getAllPlayersFromTennisclub(tennisclubId: Long): Single<List<Player>>? {
+    fun getAllPlayersFromTennisclub(tennisclubId: Long): Single<List<Player>> {
         return api.getAllPlayersFromTennisclub(tennisclubId).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).doOnSuccess{
             it.forEach {
                 Log.i("FREDSON", it.toString())
@@ -25,7 +25,7 @@ class PlayerRepository: InjectableRepository() {
     /**
      * Get [Player] from [API] from specific [Tennisclub]
      */
-    fun getPlayerFromTennisclub(tennisclubId: Long, playerId: Long): Single<Player>? {
+    fun getPlayerFromTennisclub(tennisclubId: Long, playerId: Long): Single<Player> {
         return api.getPlayerFromTennisclub(tennisclubId, playerId).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).doOnSuccess{
             Log.i("FREDSON", it.toString())
         }.doOnError {
@@ -33,7 +33,7 @@ class PlayerRepository: InjectableRepository() {
         }
     }
 
-    fun registerNewPlayerInTennisclub(tennisclubId: Long, player: Player): Single<Player>? {
+    fun registerNewPlayerInTennisclub(tennisclubId: Long, player: Player): Single<Player> {
         return api.registerNewPlayerInTennisclub(tennisclubId, player).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).doOnSuccess {
             Log.i("FREDSON", it.toString())
         }.doOnError {
@@ -41,7 +41,7 @@ class PlayerRepository: InjectableRepository() {
         }
     }
 
-    fun changePlayer(tennisclubId: Long, playerId: Long, player: Player): Single<Player>? {
+    fun changePlayer(tennisclubId: Long, playerId: Long, player: Player): Single<Player> {
         return api.changePlayer(tennisclubId, playerId, player).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).doOnSuccess{
             Log.i("FREDSON", it.toString())
         }.doOnError {
--- TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/ui/viewmodels/MatchViewModel.kt
+++ TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/ui/viewmodels/MatchViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.frederikdeprez.tennistime.ui.viewmodels
 
-class MatchViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.frederikdeprez.tennistime.data.Player
+import io.reactivex.rxkotlin.addTo
+
+class MatchViewModel: BaseViewModel() {
+
+    private val _playerList = MutableLiveData<List<Player>>()
+    val playerList: LiveData<List<Player>> = _playerList
+
+    init {
+        getAllPlayersFromTennisclub()
+    }
+
+    fun getAllPlayersFromTennisclub() {
+        playerRepository.getAllPlayersFromTennisclub(1)
+                .subscribe({
+                    _playerList.value = it
+                }, {
+                    Log.i("FREDEX", it.toString())
+                })
+                .addTo(compositeDisposable)
+    }
+
+
 }
