9eb3396244991822dfc96dec4083c93110bed96a
Frederik Deprez <deprez.frederik@gmail.com>
2019-01-06 22:00:43 +0100

    Update mainactivity to loadfragment when created

--- TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/ui/tabs/MainActivity.kt
+++ TennisTime/app/src/main/java/com/example/frederikdeprez/tennistime/ui/tabs/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity(), PlayerFragment.OnPlayerFragmentListene
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(PlayerFragment.newInstance())
     }
 
     override fun onStart() {
